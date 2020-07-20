ad1f004ba44307f8d5a48516ab8463ff00fbe992
SUSHMIT VAISH <35952453+sushmitvaish@users.noreply.github.com>
2020-07-20 16:24:17 +0530

    Add files via upload

---
+++ source_code/src/webmining/textprocessing/StopWordChecker.java
@@ -0,0 +1,29 @@
+package webmining.textprocessing;
+
+import java.util.Map;
+
+public class StopWordChecker extends AbstractChecker {
+
+    private static final String PATH_TO_CSV_FILE_CONTAINING_STOP_WORDS_LIST = BASE_PATH + "0wordLists//StopWords.csv";
+    private static final Map<String,Integer> STOP_WORDS_LIST = initializeMap();
+    private static final StopWordChecker INSTANCE = new StopWordChecker();
+    
+    private StopWordChecker(){}    
+    
+    public static StopWordChecker getInstance() {
+        
+        return INSTANCE;
+    }     
+    
+    public static boolean isTextAStopWord(String givenText){
+        
+        return STOP_WORDS_LIST.containsKey(givenText);
+    }
+    
+    private static Map initializeMap(){
+        
+        System.out.println("initializing stopwords checker");        
+        
+        return initializeMap(PATH_TO_CSV_FILE_CONTAINING_STOP_WORDS_LIST);
+    }
+}
